683fe7d51b0cb693bc295efe05d22c9783401949
Ray0Emma <ahmadifarah22@gmail.com>
2022-03-21 08:46:03 +0000

    feat: added userController

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resources('users', UserController::class);
+Route::resource('users', UserController::class);
